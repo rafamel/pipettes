62f40c462c06bd864bfa975008667444fb520a0b
Rafa Mel <melrcm@gmx.net>
2020-04-09 01:08:54 +0200

    feat: implements pipe

---
+++ src/pipe.ts
@@ -0,0 +1,37 @@
+import {
+  Pipe,
+  UnaryFunction,
+  AsyncUnaryFunction,
+  MaybeAsyncUnaryFunction,
+  AsyncPipe
+} from './types';
+
+export const pipe = Object.assign(pipeFn as Pipe<any, any>, {
+  async: asyncPipe as AsyncPipe<any, any>
+});
+
+function pipeFn(...fns: Array<UnaryFunction<any, any> | undefined>): any {
+  return fns.reduce(
+    (acc: UnaryFunction<any, any>, fn) => {
+      if (!fn) return acc;
+      return function(this: any, value: any) {
+        return fn.call(this, acc.call(this, value));
+      };
+    },
+    (value: any) => value
+  );
+}
+
+function asyncPipe(
+  ...fns: Array<MaybeAsyncUnaryFunction<any, any> | undefined>
+): any {
+  return fns.reduce(
+    (acc: AsyncUnaryFunction<any, any>, fn) => {
+      if (!fn) return acc;
+      return async function(this: any, value: any) {
+        return fn.call(this, await acc.call(this, value));
+      };
+    },
+    async (value: any) => value
+  );
+}
